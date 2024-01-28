1240682652ac847abf66ede37f4e048462000b9a
nikhilsaireddychoppa <nchoppa@uvm.edu>
2024-01-28 13:20:04 -0600

    SQL operations in Dataframes API and normal SQL spark operation completed

--- src/main/scala/pack/SparkSQLDataframeOperations.scala
+++ src/main/scala/pack/SparkSQLDataframeOperations.scala
@@ -2,6 +2,7 @@ package pack
 
 import org.apache.spark._
 import org.apache.spark.sql._
+import org.apache.spark.sql.expressions.Window
 import org.apache.spark.sql.functions._
 
 object SparkSQLDataframeOperations {
@@ -103,8 +104,158 @@ object SparkSQLDataframeOperations {
      */
     df.withColumn("condata", concat_ws("-", $"id", $"category",$"product")).select("id", "category", "product", "condata").show(numRows = 20, truncate = false)
 
+    // 16th Transformation = Lowercase data
+    /*
+    .select() is only when you want to show only few columns as output.
+    but if you want to show all columns you don't need to put .select(), directly .show()
+     */
+    df.withColumn("lower", lower($"category")).show()
+
+    // 17th Transformation = Replace Nulls data = coalesce
+    df.withColumn("replaceNulls", coalesce($"product", lit("nikhil"))).show()
+
+    // 18th Transformation = Trim the space
+    /*
+    Operation: trim specifically targets and removes any spaces at the start and end of the string. It does not affect spaces in the middle of the string.
+    Example:If a value in the product column is " Gymnastics Mat ", the trim function will change it to "Gymnastics Mat" (removing the leading and trailing spaces).
+    */
+    df.withColumn("trimmedProduct", trim($"product")).show()
+
+    // 19th Transformation = Distinct the columns
+    /*
+       distinct: This method returns a new DataFrame with distinct rows, effectively removing duplicates. It ensures that each combination of category and
+        spendby in the resulting DataFrame is unique.
+        */
+    df.select("category", "spendby").show()
+    df.select("category", "spendby").distinct().show()
+
+    // 20th Transformation = Substring with Trim
+    // pos is the starting position of substring and len is the length of substring
+    df.withColumn("sub", substring($"product",1,11)).show()
+    df.select(substring($"product", 3, 11).as("sub2")).show()
+
+    // 21th Transformation = Substring/Split operation
+    df.select(substring_index($"category"," ", 3)).show()
+    df.select(substring_index($"category", ",", 1).as("spl")).show()
+
+    // 22th Transformation = Union all
+    df.union(df1).show()
+
+    // 23rd Transformation = Union
+    df.union(df1).orderBy($"id").show()
+
+    // 24th Transformation = Aggregate Sum
+    /*
+    performing a grouping and aggregation operation on the DataFrame df.
+    It groups the data by the category column and then calculates the sum of the amount column for each category.
+    The result is a new DataFrame with two columns: category and total, where total is the sum of amount for each category.
+     */
+    df.groupBy("category")
+      .agg(sum("amount").as("total"))
+      .show()
+
+    // 25th Transformation = Aggregate sum with two columns
+    df.groupBy("category", "spendby")
+      .agg(sum("amount").as("total"))
+      .show()
+
+    // 26th Transformation = Aggregate count
+    df.groupBy("category", "spendby")
+      .agg(
+        sum("amount").as("total"),
+        count("amount").as("cnt")
+      )
+      .show()
+
+    df.show()
+
+    // 27th Transformation: Aggregate with Order Descending
+    df.groupBy("category")
+      .agg(max("amount").as("max"))
+      .orderBy($"category".desc)
+      .show()
+
+    // 28th Transformation: Window Row Number
+    /*
+    Window.partitionBy("category").orderBy($"amount".desc): Defines a window specification that partitions data by category and orders it by amount in descending order within each partition.
+    df.withColumn("row_number", row_number().over(windowSpec)): Adds a new column row_number to your DataFrame, where the row number is assigned based on the defined window specification.
+    Here row_number() is an inbuilt method in Apache Spark's SQL functionality. It's a type of window function used to assign a unique sequential integer (starting from 1) to each row within a partition of the dataset.
+     */
+    val windowSpec = Window.partitionBy("category").orderBy($"amount".desc)
+    df.withColumn("row_number", row_number().over(windowSpec)).show()
 
 
+    // 29th Transformation: Window Dense_rank Number
+    // dense_rank() will make Rows with the same values receive the same rank, and the next rank is increased by 1,
+    val windowSpec1 = Window.partitionBy("category").orderBy($"amount".desc)
+    df.withColumn("dense_rank", dense_rank().over(windowSpec1)).show()
+
+    // 30th Transformation: Window rank Number
+    val windowSpec2 = Window.partitionBy("category").orderBy($"amount".desc)
+    df.withColumn("rank", rank().over(windowSpec2)).show()
+
+    // 31st Transformation: Window Lead function
+    df.withColumn("lead", lead($"amount", 1).over(windowSpec2)).show()
+
+    // 32nd Transformation: Window LAd function
+    df.withColumn("lag", lag($"amount", 1).over(windowSpec2)).show()
+
+    // 33rd Transformation: Having functionality using agg and filter
+    df.groupBy("category").agg(count("category").as("cnt")).filter($"cnt" > 1).show()
+
+    // 34th Transformation Joins(Inner, Left, Right, Full, Left Anti)
+    cust.join(prod, cust("id") === prod("id")).select(cust("id"), cust("name"), prod("product")).show() // Inner Join
+    cust.join(prod, cust("id") === prod("id"), "left").select(cust("id"), cust("name"), prod("product")).show() // Left Join
+    cust.join(prod, cust("id") === prod("id"), "right").select(cust("id"), cust("name"), prod("product")).show() // Right Join
+    cust.join(prod, cust("id") === prod("id"), "full").select(cust("id"), cust("name"), prod("product")).show() // Full Join
+    cust.join(prod, cust("id") === prod("id"), "leftanti").select(cust("id"), cust("name")).show() // Left Anti Join
+
+    // 35th Transformation: date format
+    df.select($"id", $"tdate", date_format(to_date($"tdate", "MM-dd-yyyy"), "yyyy-MM-dd").as("con_date")).show()
+
+    // 36th Transformation: sub query
+    /*
+    Subqueries in DataFrame API are typically handled by creating temporary views or DataFrames and then performing operations on them.
+     */
+    val subDF = df.withColumn("con_date", date_format(to_date($"tdate", "MM-dd-yyyy"), "yyyy-MM-dd"))
+    subDF.groupBy("con_date").agg(sum("amount").as("total")).show()
+
+    /*
+    Date and time formatting involves various symbols to represent different components of date and time values. Here's an overview of the most commonly used format specifiers in date and time formats, particularly relevant in the context of Apache Spark and similar systems:
+
+    Date Components
+    Year:
+
+    yyyy or YYYY: Represents the year as a four-digit number (e.g., 2023).
+    yy: Represents the last two digits of the year (e.g., 23 for 2023).
+    Month:
+
+    MM: Represents the month as a two-digit number (01-12).
+    MMM: Represents the abbreviated name of the month (Jan, Feb, Mar, etc.).
+    MMMM: Full month name (January, February, etc.).
+    Day:
+
+    dd: Represents the day of the month as a two-digit number (01-31).
+    Time Components
+    Hour:
+
+    HH: Represents the hour in a 24-hour format (00-23).
+    hh: Represents the hour in a 12-hour format (01-12).
+    Minute:
+
+    mm: Represents the minute within the hour (00-59).
+    Second:
+
+    ss: Represents the second within the minute (00-59).
+    SSS: Represents milliseconds.
+    AM/PM Marker
+    AM/PM:
+    a: Represents the AM/PM marker in a 12-hour format.
+    Time Zone
+    Time Zone:
+    z: Time zone abbreviated name (e.g., PST, EST).
+    Z: Time zone offset from UTC (e.g., +0200).
+     */
 
 
   }
--- src/main/scala/pack/SparkSQLOperations.scala
+++ src/main/scala/pack/SparkSQLOperations.scala
@@ -95,11 +95,118 @@ object SparkSQLOperations {
     // 15th Transformation = concat_ws data
     spark.sql("select id, category, product, concat_ws('-', id, category, product) as condata from df").show()
 
+    // 16th Transformation = Lowercase data
+    spark.sql("select category, lower(category) as lower from df").show()
 
+    // 17th Transformation = Replace Nulls data = coalesce
+    spark.sql("select product, coalesce(product, '1000') as replaceNulls from df").show()
 
+    // 18th Transformation = Trim the space
+    /*
+    Operation: trim specifically targets and removes any spaces at the start and end of the string. It does not affect spaces in the middle of the string.
+    Example:If a value in the product column is " Gymnastics Mat ", the trim function will change it to "Gymnastics Mat" (removing the leading and trailing spaces).
+     */
+    spark.sql("select trim(product) from df").show()
 
+    // 19th Transformation = Distinct the columns
+    /*
+    distinct: This method returns a new DataFrame with distinct rows, effectively removing duplicates. It ensures that each combination of category and
+     spendby in the resulting DataFrame is unique.
+     */
+    spark.sql("select distinct category, spendby from df").show()
 
+    // 20th Transformation = Substring with Trim
+    spark.sql("select product from df")
 
+    spark.sql("select substring(product,1,10) as sub from df")
+
+    // 21th Transformation = Substring/Split operation
+    spark.sql("select substring_index(category,'',1) as spl from df").show()
+
+    // 22th Transformation = Union all
+    spark.sql("select * from df union all select * from df1").show()
+
+    // 23rd Transformation = Union
+    spark.sql("select * from df union select * from df1 order by id").show()
+
+    // 24th Transformation = Aggregate Sum
+    /*
+    performing a grouping and aggregation operation on the DataFrame df.
+    It groups the data by the category column and then calculates the sum of the amount column for each category.
+    The result is a new DataFrame with two columns: category and total, where total is the sum of amount for each category.
+     */
+    spark.sql("select category,sum(amount) as total from df group by category").show()
+
+    // 25th Transformation = Aggregate sum with two columns
+    spark.sql("select category,spendby,sum(amount) as total from df group by category,spendby").show()
+
+    // 26th Transformation = Aggregate count
+    spark.sql("select category, spendby,sum(amount) as total, count(amount) as cnt from df group by category,spendby").show()
+
+    // 27th Transformation: Aggregate with Order Descending
+    spark.sql("select category, max(amount) as max from df group by category order by category desc").show()
+
+    // 28th Transformation: Window Row Number
+    /*
+    The SQL query you're using involves a window function, specifically row_number(), in conjunction with partition by and order by.
+     This is a powerful feature in SQL used for more advanced data processing tasks.
+
+     */
+    spark.sql("select category, amount, row_number() over(partition by category order by amount desc) as row_number from df").show()
+
+    // 29th Transformation: Window Dense_rank Number
+    /*
+dense_rank() is a window function that assigns a rank to each row within a partition of a result set. The ranks are consecutive integers starting from 1. Rows with equal values for the ranking criteria receive the same rank.
+     */
+    spark.sql("select category, amount, dense_rank() over(partition by category order by amount desc) as dense_rank from df").show()
+
+    // 30th Transformation: Window rank Number
+    spark.sql("select category, amount, rank() over(partition by category order by amount desc) as rank from df").show()
+
+    // 31st Transformation: Window Lead function
+    /*
+    Purpose: The lead function is used to access data from a subsequent row in the dataset. It's similar to lag but looks forward instead of backward.
+    Syntax: lead(column, offset, defaultValue)
+    column: The column from which to fetch the value.
+    offset: The number of rows ahead of the current row from which to fetch the value. An offset of 1 refers to the immediate next row.
+    defaultValue: An optional parameter that provides a default value if the lead operation goes beyond the end of the window. If not specified, null is returned for these cases.
+     */
+    spark.sql("select category, amount, lead(amount) over(partition by category order by amount desc) as lead from df").show()
+
+    // 32nd Transformation: Window lag function
+    /*
+    Purpose: The lag function is used to access data from a previous row in the dataset without having to do a self-join. It returns the value from a column in a row that is a certain number of rows behind the current row within the same partition.
+    Syntax: lag(column, offset, defaultValue)
+    column: The column from which to fetch the value.
+    offset: The number of rows behind the current row from which to fetch the value. An offset of 1 refers to the immediate previous row.
+    defaultValue: An optional parameter that provides a default value if the lag operation goes beyond the beginning of the window. If not specified, null is returned for these cases.
+     */
+    spark.sql("select category, amount, lag(amount) over(partition by category order by amount desc) as lag from df").show()
+
+    // 33rd Transformation: Having function
+    spark.sql("select category, count(category) as cnt from df group by category having count(category)>1").show()
+
+    // 34th Transformation: Inner Join
+    spark.sql("select a.id, a.name, b.product from cust a join prod b on a.id=b.id").show()
+
+    // 35th Transformation: right Join
+    spark.sql("select a.id, a.name, b.product from cust a right join prod b on a.id=b.id").show()
+
+    // 36th Transformation: full Join
+    spark.sql("select a.id, a.name, b.product from cust a full join prod b on a.id=b.id").show()
+
+    // 37th Transformation: left anti Join
+    spark.sql("select a.id, a.name from cust a left anti join prod b on a.id=b.id").show()
+
+    // 38th Transformation: date format
+    spark.sql("select id, tdate, from_unixtime(unix_timestamp(tdate, 'mm-dd-yyyy'),'yyyy-mm-dd') as con_date from df").show()
+
+    // 39th Transformation: sub query
+    spark.sql("" +
+      "select sum(amount) as total, con_date from(" +
+      "select id, tdate, from_unixtime(unix_timestamp(tdate, 'MM-dd-yyyy'), 'yyyy-MM-dd') as con_date, amount, category, product, spendby from df)" +
+      "group by con_date" +
+      "").show()
 
   }
 
